0379d8ca51890a04aa9615770d32b0e94ff48475
Aaron LeCheminant <Aaron@Brelyns-MacBook-Pro-2.local>
2017-05-22 10:02:35 -0400

    update styles

--- src/components/CareerCard.js
+++ src/components/CareerCard.js
@@ -4,35 +4,38 @@ import React, { Component } from 'react';
 export default class CareerCard extends Component {
 
 
-  
+
   render() {
     const { name, description, type, id } = this.props.career;
     const { career, courses, opacity } = this.props;
-    const coursesNeeded = this.props.courses.filter(course =>{
-       return courses[0].careers[career.id].includes(course.id)
+    const coursesNeeded = this.props.courses.filter(course => {
+      return courses[0].careers[career.id].includes(course.id)
     })
     let styles = {
       opacity,
-      background: `url(${career.image})center center no-repeat cover`
+      // background: 
+      background: `url(${career.image})center center no-repeat`
     }
-    console.log(career.image)
 
 
     return (
-         <div 
-            style={styles}
-            className="career_card col-md-6 col-lg-4 col-sm-12 text-center">
-                <h5>{name}</h5>
-              <div className="course_container ">
-                {coursesNeeded.map(course =>{
-                  return(
-                    <p key={course.id}>{course.name}</p>
-                  )
-                })} 
-              </div>
+      <div
+        style={styles}
+        className="career_card col-md-6 col-lg-4 col-sm-12 text-center">
+        <div className="opacity_card">
+          <h5>{name}</h5>
+          <div className="course_container ">
+            <p className="selected" key={courses[0].id}>{courses[0].name}</p>
+            {coursesNeeded.map(course => {
+              return (
+                <p key={course.id}>{course.name}</p>
+              )
+            })}
+          </div>
+        </div>
 
-            </div>
+      </div>
 
-      );
+    );
   }
 }
